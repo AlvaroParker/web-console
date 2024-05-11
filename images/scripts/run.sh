17dbfa431e6004944dc66d0a4c6881a261e5e90e
Parker <aparkerdf@protonmail.com>
2024-05-11 18:02:02 -0400

    Fix: Set -e on scripts

--- images/scripts/run.sh
+++ images/scripts/run.sh
@@ -1,4 +1,5 @@
 #! /bin/sh
+set -e
 
 gcc -Wall -Wextra -Wpedantic -o /app/main /app/main.c
 /app/main
--- images/scripts/runcpp.sh
+++ images/scripts/runcpp.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
-
+set -e
 g++ -Wall -Wextra -Wpedantic -o /app/main /app/main.cpp
 
 /app/main
